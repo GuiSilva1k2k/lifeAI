3b9bd6248b8f6d11e13d23daf6b1593df085d4a8
dissectedv <joaovictorsampaio393@gmail.com>
2025-05-27 15:47:02 -0400

    ajuste cores, pag exercicios

--- front/src/app/pages/exercicios/exercicios.component.ts
+++ front/src/app/pages/exercicios/exercicios.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../../sidebar/sidebar.component';
+
+interface Exercicio {
+  nome: string;
+  gif: string;
+}
 
 @Component({
   selector: 'app-exercicios',
@@ -10,4 +15,45 @@ import { SidebarComponent } from '../../sidebar/sidebar.component';
   templateUrl: './exercicios.component.html',
   styleUrls: ['./exercicios.component.scss']
 })
-export class ExerciciosComponent {}
+export class ExerciciosComponent implements OnInit {
+  exercicios: Exercicio[] = [];
+  currentIndex: number = 0;
+
+  ngOnInit(): void {
+    this.exercicios = [
+      { nome: 'Agachamento com barra', gif: 'https://media1.tenor.com/m/pdMmsiutWkcAAAAC/gym.gif' },
+      { nome: 'Supino reto', gif: 'https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/supino-reto.gif' },
+      { nome: 'Levantamento terra', gif: 'https://www.hipertrofia.org/blog/wp-content/uploads/2023/03/barbell-sumo-deadlift.gif' },
+      { nome: 'Remada curvada', gif: 'https://www.hipertrofia.org/blog/wp-content/uploads/2023/11/dumbbell-bent-over-row.gif' },
+      { nome: 'Flexão de braço', gif: 'https://www.mundoboaforma.com.br/wp-content/uploads/2021/04/flexao-de-bracos.gif' }
+    ];
+  }
+
+  previousGif(): void {
+    if (this.currentIndex > 0) this.currentIndex--;
+  }
+
+  nextGif(): void {
+    if (this.currentIndex < this.exercicios.length - 1) this.currentIndex++;
+  }
+
+  get currentGif(): string {
+    return this.exercicios[this.currentIndex]?.gif ?? '';
+  }
+
+  get currentNome(): string {
+    return this.exercicios[this.currentIndex]?.nome ?? '';
+  }
+
+  iniciarExercicio(): void {
+    console.log(`Iniciando: ${this.currentNome}`);
+  }
+
+  finalizarExercicio(): void {
+    console.log(`Finalizado: ${this.currentNome}`);
+  }
+
+  abrirTutorial(): void {
+    console.log(`Abrindo tutorial para: ${this.currentNome}`);
+  }
+}
